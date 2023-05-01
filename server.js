898488e3c7c6f1e9d6cbe5a0b55b056f3ef0a41f
codehacker <cjj9931207@gmail.com>
2023-05-01 17:15:08 +0800

    Disconnecting Players

--- server.js
+++ server.js
@@ -3,31 +3,42 @@ var io = require('socket.io')(process.env.Port || 3000);
 var shortid = require('shortid');
 
 console.log('server started');
-var playercount = 0;
+
+var players = [];
 
 io.on('connection',function(socket){
 
-    var thisClientId = shortid.generate();
+    var thisPlayerId = shortid.generate();
 
-    console.log('client connected, broadcasting spawn, id:', thisClientId);
+    console.log('client connected, broadcasting spawn, id:', thisPlayerId);
 
-    socket.broadcast.emit('spawn',{id: thisClientId});
-    playercount++;
-    for(i=0 ; i < playercount; i++){
-        socket.emit('spawn');
-        console.log('sending spawn to new player');
-    }
+    players.push(thisPlayerId);
+
+    //这里是给所有服务器的socketd通报
+    socket.broadcast.emit('spawn',{id: thisPlayerId});
+
+    //这个是给当前用户生成已经连接的玩家角色
+    players.forEach(function(playerId){
+        if(playerId == thisPlayerId)
+            return;
+
+        socket.emit('spawn',{id:playerId})
+        console.log('sendtng spawn to new player for id: ', playerId);
+    });
 
     socket.on('move',function(data)
     {
-        data.id = thisClientId;
+        data.id = thisPlayerId;
         console.log('client move',data);
 
         socket.broadcast.emit('move',data)
     });
 
     socket.on('disconnect',function (){
-        console.log('client disconnected');
-        playercount--;
+        players.splice(players.indexOf(thisPlayerId),1);
+        console.log('client disconnected'+thisPlayerId);
+
+        socket.broadcast.emit('disconnected',{id: thisPlayerId});
+        console.log('client disconnected22222222');
     })
 })
